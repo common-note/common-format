cb69f02a65240abe1d764df20a6fafd2be9aa9cb
Haozhe <sailist@outlook.com>
2025-01-18 22:01:51 +0800

    Initial commit

---
+++ document/components/playboard.tsx
@@ -0,0 +1,12 @@
+import { useEffect, useRef, useState } from 'react';
+import React from 'react';
+
+export const EditableManually: React.FC<{}> = () => {
+
+
+  return (
+    <div>
+      hello world
+    </div>
+  );
+};
---
+++ document/rspress.config.ts
@@ -0,0 +1,26 @@
+import * as path from 'node:path';
+import { defineConfig } from 'rspress/config';
+
+export default defineConfig({
+  root: path.join(__dirname, 'docs'),
+  title: 'My Site',
+  icon: '/rspress-icon.png',
+  base: '/mds-cursor/',
+  outDir: 'dist', // 改用其他目录名称
+  route: {
+    exclude: ['custom.tsx', 'components/**/*'],
+  },
+  logo: {
+    light: '/rspress-light-logo.png',
+    dark: '/rspress-dark-logo.png',
+  },
+  themeConfig: {
+    socialLinks: [
+      {
+        icon: 'github',
+        mode: 'link',
+        content: 'https://github.com/web-infra-dev/rspress',
+      },
+    ],
+  },
+});
